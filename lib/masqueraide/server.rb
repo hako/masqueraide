2b2a33f6f14a94cb98198f4bf95481a8cac6cfc2
Wesley Hill <wesley@hakobaito.co.uk>
2016-04-10 08:11:53 +0100

    added server comments

--- lib/masqueraide/server.rb
+++ lib/masqueraide/server.rb
@@ -5,11 +5,14 @@ require 'sinatra/base'
 module Masqueraide
   # Masqueraide server powered by Sinatra.
   class Server < Sinatra::Application
-
+    
+    # We need to set the views folder to /views 
+    # so that we can load the assets when serving endpoints.
   	configure do
   		set :public_folder, File.dirname(__FILE__) + '/views'
   	end
-
+    
+    # Serve the homepage.
     get '/'	do
       erb :index
     end
